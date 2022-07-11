1bfd258d88aa97fddb7c7eee69e5acd54a21946b
Justin Bacher <32350542+Jabbey92@users.noreply.github.com>
2022-07-11 14:48:34 -0500

    🗑️Remove server code from server to dev server

--- pynetic/server/server.py
+++ pynetic/server/server.py
@@ -3,17 +3,10 @@
 Hot reloads pages on save
 """
 
-from starlette.applications import Starlette
-from starlette.responses import JSONResponse
-from starlette.routing import Route
-import uvicorn
-
-from ..core import Session, Reference
-from ..core.html import HTMLElement
+from .dev_server import Application
 
 def application_loop():
     app = Starlette(debug=True)
-    while True:
 
 if __name__ == '__main__':
     uvicorn.run("server:run_server", host="127.0.0.1", port=8000, log_level="debug")
