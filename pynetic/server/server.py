d3dee53be57eb6f8423daa2d490fc166bc64261f
Jabbey92 <92jbach@gmail.com>
2022-07-12 21:55:05 -0500

    💥possible hot reloading partial implementation

--- pynetic/server/server.py
+++ pynetic/server/server.py
@@ -3,10 +3,15 @@
 Hot reloads pages on save
 """
 
-from .dev_server import Application
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+from starlette.routing import Route
+import uvicorn
+
 
 def application_loop():
     app = Starlette(debug=True)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     uvicorn.run("server:run_server", host="127.0.0.1", port=8000, log_level="debug")
---
+++ pynetic/server/starlette.py
@@ -0,0 +1,45 @@
+""""""
+
+from importlib import import_module, reload
+from os import path
+from sys import modules
+from pathlib import Path
+from types import FunctionType, ModuleType
+from typing import AsyncGenerator, cast
+
+from ..core.page import Page
+from ..core import Session, Reference
+from ..core.html import HTMLElement
+
+current_working_directory = Path("./routes/")
+
+
+class Transpiler:
+    def __init__(self) -> None:
+        self.files: dict[str, tuple[float, Page]] = {}
+        pass
+
+    async def sync_routes(self) -> None:
+        """Loops through routes folder returning all python filepaths returning their relative link
+
+        Automatically reloads the route on file change, add, or remove. And removes route on file deletion.
+        """
+        for file in current_working_directory.glob("**.py"):
+            last_modified_date = file.stat().st_mtime
+            route_name = f"/{file.relative_to('.').stem}"
+
+            if route_name not in self.files:
+                import_module((filename := file.name))
+                for local in (module := cast(ModuleType, eval(route_name))).items():
+                    if isinstance(local, Page):
+                        self.files[route_name] = last_modified_date, local
+                        break
+                else:
+                    raise Warning(
+                        f"{filename} Does not contain a Page element. This module was not loaded."
+                    )
+            else:
+                module = cast(ModuleType, eval(route_name))
+
+            if self.files[route_name] != last_modified_date:
+                reload(module)
