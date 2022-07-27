6ec8af5d1c7975bbb245e68ec501532d7d60787b
Jabbey92 <92jbach@gmail.com>
2022-07-27 18:10:50 -0500

    ✨Start working on build steps

--- pynetic/core/application.py
+++ pynetic/core/application.py
@@ -1,5 +1,9 @@
 """Represents a client session"""
 
+from pathlib import Path
+from importlib import import_module
+
+from .component import Component
 from .reference import Reference
 
 
@@ -9,4 +13,16 @@ class Application:
     references: dict[str, Reference] = {}
 
     def __init__(self) -> None:
-        pass
+        self._components = set()
+
+    def build(self) -> str | None:
+        routes_folder = Path().absolute().joinpath("routes")
+
+        for route_path in routes_folder.glob("*.py"):
+            route = import_module(str(route_path))
+            component = next(
+                component
+                for component in reversed(route.__dict__.values())
+                if isinstance(component, Component)
+            )
+            self._components.add(component)
