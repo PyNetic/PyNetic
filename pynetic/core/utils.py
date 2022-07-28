113d8be220b6ca7905ab9653ac0a6a62b06187fa
Jabbey92 <92jbach@gmail.com>
2022-07-27 19:00:06 -0500

    🎨Formatting

--- pynetic/core/utils.py
+++ pynetic/core/utils.py
@@ -8,14 +8,15 @@ from typing import Any, Callable, Iterable, TypeVar
 
 all_letters = ascii_lowercase + ascii_uppercase
 
+
 class For:
     """Replacement for builtin for loop to use inline with DOM elements
-    
+
     Args:
         each (Iterable): The iterable to loop over
         condition (Callable | None): The function to use as a condition in the for loop
         do (Callable): What to do in the loop
-    
+
     Usage:
         ```Python
         my_list = ["Button 1", "Button 2", "Button 3"]
@@ -25,15 +26,15 @@ class For:
                 lambda x: x != 2,
                 lambda name: Button(name)
             )
-                
+
         )
     """
+
     def __init__(self, each: Iterable, condition: Callable | None, statements: Any, /) -> None:
         self.each = each
         self.condition = condition
         self.statements = statements
-        
-    
+
 
 def iter_short_names():
     """Generator over a-z ... A-Z ... aa-ZZ ..."""
