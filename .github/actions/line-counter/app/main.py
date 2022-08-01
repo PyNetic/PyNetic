bf73cf136b3a4f7ba674b20560d76e4a9aff59c6
Jabbey92 <92jbach@gmail.com>
2022-08-01 18:12:01 -0500

    📈 Revert to old sha formatting

--- .github/actions/line-counter/app/main.py
+++ .github/actions/line-counter/app/main.py
@@ -1,47 +1,41 @@
 import re
 from os import environ
-from typing import cast
 
 import requests
-from github import Github
-from github.ContentFile import ContentFile
-from github.GithubException import GithubException
+from github import Github, GithubException
 from mdutils import MdUtils
 
-REPO_NAME = cast(str, environ.get("GITHUB_REPOSITORY"))
+print(f">>> Starting Code Stats Process for {(REPO_NAME := environ.get('GITHUB_REPOSITORY', ''))}")
 
-print(f">>> Starting Code Stats Process for {REPO_NAME} <<<")
-
-PROJECT_NAME = REPO_NAME.split("/")[-1]
+# Setup Constants
 OUT_PATH = ".github/stats/Code Statistics.md"
 LOC_API_URL = f"https://api.codetabs.com/v1/loc?github={REPO_NAME}"
-REPOSITORY = Github(environ.get("INPUT_GITHUB_TOKEN")).get_repo(REPO_NAME)
-COMMIT = REPOSITORY.get_commit(environ.get("GITHUB_SHA"))
-data = map(dict.values, requests.get(LOC_API_URL).json())
-# Setup Tables
 KEYS = ["📝Files", "〰️Lines", "🗨️Blanks", "🙈Comments", "👨‍💻Lines of Code"]
+
+# Setup globals
+repository = Github(environ.get("INPUT_GITHUB_TOKEN")).get_repo(REPO_NAME)
 languages_table = ["", *KEYS]
 language_chart_table = {}
 lines_chart_table = []
 
-if isinstance(COMMIT, list):
-    COMMIT = COMMIT[0]
-
 # Create Markdown File
 md_file = MdUtils("Lines Of Code.md")
 md_file.create_md_file()
-md_file.new_header(1, f"📊 Code Statistics for {PROJECT_NAME}")
+md_file.new_header(1, f"📊 Code Statistics for {REPO_NAME.split('/')[-1]}")
 
 # Populate Languages Table
-for num_languages, language in enumerate(data, 1):
-    lang, *_, lines = language
+for num_languages, language in enumerate(map(dict.values, requests.get(LOC_API_URL).json()), 1):
+    lang, *language_values = (lang, *_, lines) = language
     languages_table.extend(language)
 
     if lang == "Total":
-        lines_chart_table.extend([*_, lines])
+        lines_chart_table.extend(language_values)
         break
 
     language_chart_table[lang] = lines
+
+else:
+    num_languages = 0
 
 # Add Languages Pie Chart
 md_file.new_line("```mermaid")
@@ -70,19 +64,28 @@ md_file.new_table(columns=6, rows=num_languages + 1, text=languages_table)
 md_file.new_line()
 
 # Updated contents for markdown file
-new_contents = re.sub(r"\s{2}$(?<!\d)", "", md_file.get_md_text(), flags=re.M)[1:]
+# For some reason mdutils insists on putting 2 lines
+# after every new line. I'm just taking those lines
+# out so mermaid formats correctly
+new_content = re.sub(r"\s{2}$(?<!\d)", "", md_file.get_md_text(), flags=re.M)[1:]
 
 # Update Readme
 try:
-    REPOSITORY.update_file(
+    repository.update_file(
         OUT_PATH,
         "📈 Update stats file",
-        new_contents,
-        COMMIT.sha,
+        new_content,
+        (
+            content[0]
+            if isinstance(
+                content := repository.get_contents(OUT_PATH),
+                list,
+            )
+            else content
+        ).sha,
     )
 
 except GithubException as err:
-    print(f"Could not edit file because of this error: {err}")
-    REPOSITORY.create_file(OUT_PATH, "🎉 Create stats file", new_contents)
+    repository.create_file(OUT_PATH, "🎉 Create stats file", new_content)
 
-print(f">>> Code Stats Process for {REPO_NAME} Finished <<<")
+print(f">>> Code Stats Process for {REPO_NAME} Finished")
